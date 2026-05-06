3e5a539266b09ca448f7dabbd3ba5ef288f8123c
Lucas Pham <phamquanghop.ict@gmail.com>
2026-05-06 09:49:19 +0700

    delete

--- src/ring_oscillator_9stage.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-(* keep_hierarchy = "yes" *)
-module ring_oscillator_9stage (input wire clk, input wire en, output reg output_ro);
-    (* KEEP = "TRUE", DONT_TOUCH = "TRUE", ALLOW_COMBINATORIAL_LOOPS = "TRUE" *) wire rot, nad, iv1, iv2, iv3, iv4, iv5, iv6, iv7, iv8;
-    assign nad = (en & rot); assign iv1 = ~nad; assign iv2 = ~iv1; assign iv3 = ~iv2; assign iv4 = ~iv3;
-    assign iv5 = ~iv4; assign iv6 = ~iv5; assign iv7 = ~iv6; assign iv8 = ~iv7; assign rot = ~iv8;
-    always @(posedge clk) output_ro <= rot;
-endmodule
